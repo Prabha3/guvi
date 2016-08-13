3c9cf28bcbd83866761070b52dc73d116960d474
Prabha3 <prabhaamani@gmail.com>
2016-08-13 11:51:24 +0530

    Add files via upload

---
+++ B1.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+public class B1
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);		
+		System.out.println("Enter the n values:");
+		if(n<0)
+		{
+			System.out.println("Negative");
+		}
+		elseif(n==0)
+		{
+			
+			System.out.println("Zero");
+		}
+		else
+		{
+			
+			System.out.println("Postive");
+		}
+	}
+}
