56bf2e49a7ecb2185ca6352b17aa35478c05ac71
Prabha3 <prabhaamani@gmail.com>
2016-08-13 12:12:56 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+public class P1
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		StringBuffer stb=new StringBuffer(s.next());
+		stb.reverse();
+		System.out.println(stb);
+	}
+}
