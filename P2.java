8fc54ef5898c49dbac3cff77c92e0422f77cc652
Prabha3 <prabhaamani@gmail.com>
2016-08-13 12:20:29 +0530

    Add files via upload

---
+++ P2.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+public class P2
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the n:");
+		int i,fact=1;
+		for(i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+}
