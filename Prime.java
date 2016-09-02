10c914f2013abe835798ad235af0b0ff6c972759
Prabha3 <prabhaamani@gmail.com>
2016-09-02 11:12:03 +0530

    Add files via upload

--- Prime.java
+++ Prime.java
@@ -1,26 +1,25 @@
-import java.util.*;
-import java.io.*;
 import java.lang.*;
+import java.io.*;
+import java.util.*;
 public class Prime
 {
-public static void main(String arg[])
-{
-Scanner s=new Scanner(System.in);
-int st=s.nextInt();
-int ed=s.nextInt();
-int i=0,j=0;
-int f;
-for(i=st;i<=ed;i++)
-{
-f=0;
-for(j=2;j<i;j++)
-{
-if(i%j==0)
-  f=1;
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int st=s.nextInt();
+		int ed=s.nextInt();
+		int i=0;j=0;
+		int f;
+		for(i=st;i<=ed;i++)
+		{
+			f=0;
+			for(j=2;j<i;j++)
+			{
+				if(i%j==0)
+					f=1;
+			}
+			if(f==0)
+				System.out.println(i);
+		}
+	}
 }
-if(f==0)
-System.out.println(i);
-}
-}
-}
-  
