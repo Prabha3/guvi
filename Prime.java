d9701f3d9a0ed3ba0c3dbebf0cd6c0edb7fff4d8
Prabha3 <prabhaamani@gmail.com>
2016-09-02 11:06:43 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Prime
+{
+public static void main(String arg[])
+{
+Scanner s=new Scanner(System.in);
+int st=s.nextInt();
+int ed=s.nextInt();
+int i=0,j=0;
+int f;
+for(i=st;i<=ed;i++)
+{
+f=0;
+for(j=2;j<i;j++)
+{
+if(i%j==0)
+  f=1;
+}
+if(f==0)
+System.out.println(i);
+}
+}
+}
+  
